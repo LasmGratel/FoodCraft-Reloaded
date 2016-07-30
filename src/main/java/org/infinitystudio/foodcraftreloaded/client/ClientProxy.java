99121a5786502accdaea008ff147ef722623bf98
Lasm Gratel <lasm_gratel@hotmail.com>
2016-07-30 18:49:48 +0800

    rearrrrrange codes (reload the reloaded version)

--- src/main/java/org/infinitystudio/foodcraftreloaded/client/ClientProxy.java
+++ src/main/java/org/infinitystudio/foodcraftreloaded/client/ClientProxy.java
@@ -1,17 +1,17 @@
 /**
  * FoodCraft Mod for Minecraft.
  * Copyright (C) 2016 Infinity Studio.
- * <p/>
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p/>
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -19,16 +19,22 @@
  */
 package org.infinitystudio.foodcraftreloaded.client;
 
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.infinitystudio.foodcraftreloaded.common.CommonProxy;
 
-
 public class ClientProxy extends CommonProxy {
-    @Override
-    public void postInit(FMLPostInitializationEvent event) {
-        super.postInit(event);
-        manager.preInitClient();
-        manager.initClient();
-        manager.postInitClient();
+    public void preInitClient(FMLPreInitializationEvent event) {
+    }
+
+    public void initClient(FMLInitializationEvent event) {
+    }
+
+    public void postInitClient(FMLPostInitializationEvent event) {
+    }
+
+    public void loadCompleteClient(FMLLoadCompleteEvent event) {
     }
 }
--- src/main/java/org/infinitystudio/foodcraftreloaded/common/CommonProxy.java
+++ src/main/java/org/infinitystudio/foodcraftreloaded/common/CommonProxy.java
@@ -1,17 +1,17 @@
 /**
  * FoodCraft Mod for Minecraft.
  * Copyright (C) 2016 Infinity Studio.
- * <p/>
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p/>
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p/>
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
@@ -23,54 +23,17 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.infinitystudio.foodcraftreloaded.FoodCraftReloaded;
-import org.infinitystudio.foodcraftreloaded.utils.modmanagent.ModManager;
-import org.infinitystudio.foodcraftreloaded.utils.modmanagent.common.CommonModManagement;
 
 public class CommonProxy {
-    protected static final ModManager manager = new ModManager(
-            FoodCraftReloaded.MODID, FoodCraftRegistration.class,
-            CommonModManagement.BLOCK, CommonModManagement.BLOCKFLUID,
-            CommonModManagement.EVENT, CommonModManagement.EVENTBUS,
-            CommonModManagement.FLUID,
-            CommonModManagement.ITEM, CommonModManagement.FOOD,
-            CommonModManagement.BEAN, CommonModManagement.SPROUTS, CommonModManagement.BEANBLOCK,
-            CommonModManagement.FRUIT, CommonModManagement.JUICE, CommonModManagement.FRUITICECREAM,
-            CommonModManagement.VEGETABLE, CommonModManagement.MEAT,
-            CommonModManagement.OBJPREINIT, CommonModManagement.OBJINIT, CommonModManagement.OBJPOSTINIT,
-            CommonModManagement.TILEENTITY, CommonModManagement.FRUITSODA, CommonModManagement.JUICEFLUID,
-            CommonModManagement.BLOCKJUICEFLUID, CommonModManagement.VEGETABLEBLOCK
-    );
-
-    /**
-     * FML Pre Initialization Event Handler.
-     *
-     * @param event FMLPreInitializationEvent
-     */
     public void preInit(FMLPreInitializationEvent event) {
-        manager.preInit();
     }
 
-    /**
-     * FML Initialization Event Handler.
-     *
-     * @param event FMLInitializationEvent
-     */
     public void init(FMLInitializationEvent event) {
-        manager.init();
     }
 
-    /**
-     * FML Post Initialization Event Handler.
-     *
-     * @param event FMLPostInitializationEvent
-     */
     public void postInit(FMLPostInitializationEvent event) {
-        manager.postInit();
     }
 
     public void loadComplete(FMLLoadCompleteEvent event) {
-        FcRecipeRegistration.register();
-        FcLocalizationHelper.register();
     }
 }
