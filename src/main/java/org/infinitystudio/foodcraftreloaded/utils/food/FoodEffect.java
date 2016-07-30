f4af52745ea5b418f2d3bdb9094648553d66ef20
Lasm Gratel <lasm_gratel@hotmail.com>
2016-07-30 21:53:05 +0800

    json food start

---
+++ src/main/java/org/infinitystudio/foodcraftreloaded/utils/food/FoodEffect.java
@@ -0,0 +1,41 @@
+/**
+ * Infinity Launcher for Minecraft.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.infinitystudio.foodcraftreloaded.utils.food;
+
+/**
+ * An effect appears after ate the food.
+ */
+public class FoodEffect {
+    private String effectName;
+    private int duration;
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getEffectName() {
+        return effectName;
+    }
+
+    public void setEffectName(String effectName) {
+        this.effectName = effectName;
+    }
+}
